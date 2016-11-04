da21c44d2f1b4a9498353c012d450e991d3ba1b7
Robert <rob89m@icloud.com>
2016-11-04 13:13:59 +1100

    no message

--- Clear_Print_Queue.ps1
+++ Clear_Print_Queue.ps1
@@ -3,9 +3,13 @@ $PRINTSVR = "MIT-Robert-0916"
 
 
 # Stop 'Print Spooler' service on Print Server
+Write-Host "Stopping the Print Spooler on $PRINTSVR"
 sc.exe \\$PRINTSVR stop spooler
 
 # Remove all queued print job
+Write-Host "Clearing the Print Queue on $PRINTSVR"
 Remove-Item \\$PRINTSVR\c$\WINDOWS\System32\spool\printers\*
 
+# Start 'Print Spooler' service on Print Server
+Write-Host "Starting the Print Spooler on $PRINTSVR"
 sc.exe \\$PRINTSVR start spooler
